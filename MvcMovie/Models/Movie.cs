1ba54381f0ac196744a88dbfad92a6283bb4fb34
jose junior <jose.junior@digitalfactory.co.ao>
2021-05-12 23:38:45 +0100

    Adicionar arquivos de projeto.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [StringLength(5)]
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public string Rating { get; set; }
+
+
+    }
+}
